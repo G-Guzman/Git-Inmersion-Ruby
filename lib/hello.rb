d58dd45ae837f70e7f67adc5783d7d90be1977a7
Gael Guzman <jg.guzman21@info.uas.edu.mx>
2025-10-25 17:30:50 -0700

    Hello uses Greeter

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,5 +1,8 @@
+require 'greeter'
+
 # Default is "World"
 #Author: Gael Guzman (jg.guzman21@info.uas.edu.mx)
 name = ARGV.first || "World"
 
-puts "Hello, #{name}!"
+greeter = Greeter.new(name)
+puts greeter.greet
